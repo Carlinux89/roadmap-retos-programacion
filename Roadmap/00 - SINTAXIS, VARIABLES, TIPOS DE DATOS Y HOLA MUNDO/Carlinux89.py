e1e49646cc3e5368cf0a195186bf942a00f1eaf0
Carlinux89 <carlosj_1912_17@hotmail.com>
2024-06-29 15:12:30 -0500

    #00 - python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/Carlinux89.py
@@ -0,0 +1,26 @@
+# https://www.python.org/
+# esta es otra forma de comentario
+'''
+para comentar varias lineas o parrafos
+'''
+"""
+otra forma de comentar varias lineas o parrafos
+"""
+varaiable1="Hola mundo"
+variable1="Hola! mundo.."
+
+constante=" mi constante"
+
+entero= int(1)
+real= float(1.5)
+bol= bool(true)
+bol= bool(false)
+caracter= str("cadena de texto")
+caracter1=srt('cadena de texto')
+
+print("Hola! Ptyhon...")
+
+
+
+
+
