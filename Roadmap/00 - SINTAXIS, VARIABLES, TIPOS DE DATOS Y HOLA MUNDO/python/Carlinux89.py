9718ab391ddada5bf0832a71d2421c1bfd5f6303
Carlinux89 <carlosj_1912_17@hotmail.com>
2024-06-29 15:33:36 -0500

    Update Carlinux89.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Carlinux89.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Carlinux89.py
@@ -1,1 +1,24 @@
+# https://www.python.org/
+# 1era forma de hacer comentario
+"""
+2da forma de hacer comentario en varias lineas
+o bloques de codigo
+"""
+'''
+3ra forma de hacer comentario en varias lineas
+o bloques de codigo
+'''
+variable1="hola mundo" # es una variable de tipo cadena
+variable1="Hola! mundo.." # una variable que se puede modificar
 
+constante="mi constante"
+
+# tipos de datos 
+num=int(5) # tipo de dato entero
+rea=float(5.5) # tipo de dato flotante/decimal
+bool=True# tipo de dato boleano, regresa un valor verdadero
+bool=False # tipo de dato boleano, regresa un valor falso
+texto=str("cadena de texto") # tipo de dato cadena
+texto=str('cadena de texto') # tipo de dato cadena
+
+print("Hola! Ptyhon..")
